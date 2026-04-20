b93a0072fb4b04e429cec61d9637389606043229
Daniel Cook <danielpc@iastate.edu>
2026-04-20 13:17:41 -0500

    fixes

--- iba/jest.config.js
+++ iba/jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  rootDir: '.',
   testEnvironment: 'jsdom',
-  setupFilesAfterFramework: ['@testing-library/jest-dom'],
-  setupFilesAfterFramework: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '\\.(css|less|scss)$': '<rootDir>/__mocks__/styleMock.js',
   },
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/../backend/'],
 };
