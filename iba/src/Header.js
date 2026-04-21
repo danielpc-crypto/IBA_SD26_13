74f3b9bdced11aaa91aac2eaf5fb06a8a7909bef
Miranda Harris <mjharris8515@gmail.com>
2026-04-21 02:42:23 -0500

    adding the popover for the nav bar

--- iba/src/Header.js
+++ iba/src/Header.js
@@ -3,14 +3,27 @@ import { useNavigate } from 'react-router-dom';
 import {
   Button,
   Container,
+  Box,
+  Toolbar,
+  Avatar,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
   Dialog,
   DialogTitle,
   DialogContent,
   DialogContentText,
-  DialogActions
+  DialogActions,
+  IconButton,
+  AppBar,
+  CssBaseline,
 } from '@mui/material';
 
-function Header(){ 
+import Menu from '@mui/icons-material/Menu';
+
+function Header({open, setOpen}){ 
     const navigate = useNavigate();
     const [logoutOpen, setLogoutOpen] = useState(false);
 
@@ -22,20 +35,18 @@ function Header(){
 
     return (
         <div>
-            <div style={{ marginBottom: "20px"}}>
+            <div style={{ marginBottom: "60px"}}>
                 <Container>
-                    <nav className="navbar navbar-expand-lg fixed-top bg-body-tertiary" data-bs-theme="dark">
+                    <nav className="navbar navbar-expand-lg fixed-top bg-body-tertiary" style={{width: open ? 'calc(100% - 240px)' : '100%', marginLeft: open ? '240px' : '0px', transition: 'margin 0.3s ease',}} data-bs-theme="dark">
                         <div className="container-fluid">
+                            <IconButton color="inherit" aria-label="open drawer" onClick={()=>setOpen(!open)} edge="start" sx={{ mr: 2 }}>
+                                <Menu />
+                            </IconButton>
                             <a className="navbar-brand" href="#" onClick={(e) => { e.preventDefault(); setLogoutOpen(true); }}>Intelligent Business Analyzer</a>
                             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                                 <span className="navbar-toggler-icon"></span>
                             </button>
                             <div className="collapse navbar-collapse" id="navbarNav">
-                                <ul className="navbar-nav">
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="/dashboard">Dashboard</a>
-                                    </li>
-                                </ul>
                                 <div className="collapse navbar-collapse">
                                     <ul className="navbar-nav ms-auto">
                                         <li className="nav-item dropdown">
@@ -55,6 +66,36 @@ function Header(){
                         </div>
                     </nav>
                 </Container>
+                {/* <Box sx={{ display: 'flex'}}>
+                    <CssBaseline />
+                    <AppBar position="static" sx={{ backgroundColor: '#1a73e8' }}>
+                        <Toolbar variant="dense">
+                            <IconButton color="inherit" aria-label="open drawer" onClick={handleDrawerOpen} edge="start" sx={{ mr: 2, display: { sm: 'none' } }}>
+                                <Menu />
+                            </IconButton>
+                        </Toolbar>
+                    </AppBar> */}
+
+                    <Drawer anchor="left" variant="persistent" open={open} sx={{width: 240, flexShrink: 0, '& .MuiDrawer-paper': { width: 240,boxSizing: 'border-box',},}}>
+                        <List>
+                            <ListItem disablePadding>
+                                <ListItemButton>
+                                    <Avatar src="pictures/default.png" alt="Profile" onClick={() => navigate("/profile")} sx={{ width: 60, height: 60, backgroundColor: "#212529"}} />
+                                </ListItemButton>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemButton>
+                                    <ListItemText primary="Dashboard" onClick={() => navigate("/dashboard")} />
+                                </ListItemButton>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemButton>
+                                    <ListItemText primary="IBA Assistant" onClick={() => navigate("/assistant")} />
+                                </ListItemButton>
+                            </ListItem>
+
+                        </List>
+                    </Drawer>
             </div>
 
             <Dialog open={logoutOpen} onClose={() => setLogoutOpen(false)}>
