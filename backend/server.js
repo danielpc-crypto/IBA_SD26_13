5541d643d620ff9b018ea0866c3d8619f9133928
Daniel Cook <danielpc@iastate.edu>
2026-04-23 15:51:22 -0500

    e

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,10 @@ const genAI = new GoogleGenerativeAI(key);
 const AWS = require("aws-sdk");
 
 const db = mysql.createConnection({
-    host:"iba-database.cpy6gmwayev0.us-east-2.rds.amazonaws.com",
-    user:"admin",
-    password:"GaffarIBA123",
-    database:"iba"
+    host: "iba-database.cpy6gmwayev0.us-east-2.rds.amazonaws.com",
+    user: "admin",
+    password: "GaffarIBA123",
+    database: "iba"
 });
 
 const s3 = new AWS.S3({
